d8332658da61fdd6ef3d74938bed356a2d9aef18
Artem Kuzko <a.kuzko@gmail.com>
2019-04-20 18:19:17 +0300

    add tests for useForm hook
    add infra for tests and cover `useForm` hook usage

---
+++ test/components/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+Input.defaultProps = {
+  wrapperClassName: '',
+  errorClassName: 'error'
+};
+
+export default function Input({ value, onChange, error, name, wrapperClassName, errorClassName, ...rest }) {
+  const handleChange = e => onChange(e.target.value, e);
+
+  return (
+    <div className={ wrapperClassName }>
+      <input value={ value } onChange={ handleChange } name={ name } { ...rest } />
+      { error &&
+        <div className={ errorClassName }>{ error }</div>
+      }
+    </div>
+  );
+}
---
+++ test/useForm.js
@@ -0,0 +1,303 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import { useForm, defineValidations } from '../src';
+
+import Input from './components/Input';
+
+defineValidations({
+  presence(value) {
+    if (!value || (Array.isArray(value) && !value.length)) return "Can't be empty";
+  },
+  numericality(value, { lessThan }) {
+    if (!value) return;
+
+    if (+value >= lessThan) return `Should be less than ${lessThan}`;
+  }
+});
+
+describe('useForm', () => {
+  function Form() {
+    const { $, validate } = useForm({ foo: 'foo' });
+
+    return (
+      <div>
+        <Input { ...$('foo') } className="foo" />
+        <button onClick={ validate }>Validate</button>
+      </div>
+    );
+  }
+
+  it('uses initial value', () => {
+    const wrapper = mount(<Form />);
+    expect(wrapper.find('input.foo[value="foo"]')).to.have.lengthOf(1);
+  });
+
+  it('handles input value change', () => {
+    const wrapper = mount(<Form />);
+    wrapper.find('input.foo').simulate('change', { target: { value: 'foo2' } });
+    expect(wrapper.find('input.foo[value="foo2"]')).to.have.lengthOf(1);
+  });
+
+  describe('custom onChange handlers', () => {
+    function Form() {
+      const { $, set } = useForm({ foo: '' });
+
+      return (
+        <div>
+          <Input { ...$('foo', value => set('foo', value.toUpperCase())) } className="foo" />
+        </div>
+      );
+    }
+
+    it('allows to use custom onChange handler', () => {
+      const wrapper = mount(<Form />);
+      wrapper.find('input.foo').simulate('change', { target: { value: 'foo' } });
+      expect(wrapper.find('input.foo[value="FOO"]')).to.have.lengthOf(1);
+    });
+
+    describe('setting multiple values at once', () => {
+      function Form() {
+        const { $, set } = useForm({ foo: '', bar: '' });
+
+        const changeFoo = value => set({ foo: value, bar: value + 'bar' });
+
+        return (
+          <div>
+            <Input { ...$('foo', changeFoo) } className="foo" />
+            <Input { ...$('bar') } className="bar" />
+          </div>
+        );
+      }
+
+      it('allows to use custom onChange handler', () => {
+        const wrapper = mount(<Form />);
+        wrapper.find('input.foo').simulate('change', { target: { value: 'foo' } });
+        expect(wrapper.find('input.bar[value="foobar"]')).to.have.lengthOf(1);
+      });
+    });
+  });
+
+  describe('validations', () => {
+    function Form() {
+      const { $, validate } = useForm({ foo: '' }).withValidation({
+        foo: 'presence'
+      });
+
+      return (
+        <div>
+          <Input { ...$('foo') } className="foo" />
+          <button onClick={ validate } className="validate">Validate</button>
+        </div>
+      );
+    }
+
+    it('provides `validate` helper', () => {
+      const wrapper = mount(<Form />);
+      expect(wrapper.find('.error')).to.have.lengthOf(0);
+      wrapper.find('.validate').simulate('click');
+      expect(wrapper.find('.error')).to.have.lengthOf(1);
+    });
+
+    it('does not initially validate input on change', () => {
+      const wrapper = mount(<Form />);
+      wrapper.find('input.foo').simulate('change', { target: { value: '' } });
+      expect(wrapper.find('.error')).to.have.lengthOf(0);
+    });
+
+    it('validates input on change after validations have been explicitly ran', () => {
+      const wrapper = mount(<Form />);
+      wrapper.find('input.foo').simulate('change', { target: { value: '' } });
+      wrapper.find('.validate').simulate('click');
+      expect(wrapper.find('.error')).to.have.lengthOf(1);
+      wrapper.find('input.foo').simulate('change', { target: { value: '1' } });
+      expect(wrapper.find('.error')).to.have.lengthOf(0);
+      wrapper.find('input.foo').simulate('change', { target: { value: '' } });
+      expect(wrapper.find('.error')).to.have.lengthOf(1);
+    });
+
+    describe('validation and custom onChange handler', () => {
+      function Form() {
+        const { $, set, validate } = useForm({ foo: '' }).withValidation({
+          foo: 'presence'
+        });
+
+        return (
+          <div>
+            <Input { ...$('foo', value => set('foo', value.toUpperCase())) } className="foo" />
+            <button onClick={ validate } className="validate">Validate</button>
+          </div>
+        );
+      }
+
+      it('validates on change with custom onChange handler', () => {
+        const wrapper = mount(<Form />);
+        wrapper.find('.validate').simulate('click');
+        expect(wrapper.find('.error')).to.have.lengthOf(1);
+        wrapper.find('input.foo').simulate('change', { target: { value: 'foo' } });
+        expect(wrapper.find('.error')).to.have.lengthOf(0);
+        expect(wrapper.find('input.foo[value="FOO"]')).to.have.lengthOf(1);
+      });
+    });
+
+    describe('complex forms of value validation', () => {
+      function Form() {
+        const { $, validate } = useForm({ foo: '' }).withValidation({
+          foo: [
+            'presence',
+            { numericality: { lessThan: 10 } },
+            function(value) {
+              if (+value === 5) {
+                return 'Not five';
+              }
+            }]
+        });
+
+        return (
+          <div>
+            <Input { ...$('foo') } className="foo" />
+            <button onClick={ validate } className="validate">Validate</button>
+          </div>
+        );
+      }
+
+      it('allows to use complex validation', () => {
+        const wrapper = mount(<Form />);
+        wrapper.find('.validate').simulate('click');
+        expect(wrapper.find('.error').text()).to.eq("Can't be empty");
+        wrapper.find('input.foo').simulate('change', { target: { value: '11' } });
+        expect(wrapper.find('.error').text()).to.eq('Should be less than 10');
+        wrapper.find('input.foo').simulate('change', { target: { value: '5' } });
+        expect(wrapper.find('.error').text()).to.eq('Not five');
+        wrapper.find('input.foo').simulate('change', { target: { value: '3' } });
+        expect(wrapper.find('.error')).to.have.lengthOf(0);
+      });
+    });
+
+    describe('validating multiple values on setting them at once', () => {
+      function Form() {
+        const { $, set, validate } = useForm({ foo: '', bar: '' }).withValidation({
+          foo: 'presence',
+          bar: 'presence'
+        });
+
+        const changeFoo = value => set({ foo: value, bar: value + 'bar' });
+
+        return (
+          <div>
+            <Input { ...$('foo', changeFoo) } className="foo" errorClassName="foo-error" />
+            <Input { ...$('bar') } className="bar" errorClassName="bar-error" />
+
+            <button onClick={ validate } className="validate">Validate</button>
+          </div>
+        );
+      }
+
+      it('runs all validations to validate all changes', () => {
+        const wrapper = mount(<Form />);
+        wrapper.find('.validate').simulate('click');
+        expect(wrapper.find('.foo-error')).to.have.lengthOf(1);
+        expect(wrapper.find('.bar-error')).to.have.lengthOf(1);
+        wrapper.find('input.foo').simulate('change', { target: { value: 'foo' } });
+        expect(wrapper.find('.foo-error')).to.have.lengthOf(0);
+        expect(wrapper.find('.bar-error')).to.have.lengthOf(0);
+      });
+    });
+
+    describe('value-dependent validations', () => {
+      function Form() {
+        const { $, validate } = useForm({ foo: '', bar: '1' }).withValidation(get => {
+          if (+get('bar') > 1) {
+            return { foo: 'presence' };
+          }
+          return {};
+        });
+
+        return (
+          <div>
+            <Input { ...$('foo') } className="foo" />
+            <Input { ...$('bar') } className="bar" />
+            <button onClick={ validate } className="validate">Validate</button>
+          </div>
+        );
+      }
+
+      it('provides `get` helper to define validations', () => {
+        const wrapper = mount(<Form />);
+        wrapper.find('.validate').simulate('click');
+        expect(wrapper.find('.error')).to.have.lengthOf(0);
+        wrapper.find('input.bar').simulate('change', { target: { value: '2' } });
+        wrapper.find('input.foo').simulate('change', { target: { value: '' } });
+        expect(wrapper.find('.error')).to.have.lengthOf(1);
+      });
+    });
+
+    describe('complex validation with wildcards', () => {
+      function Form() {
+        const { get, set, $, getError, validate } = useForm({ foos: [] }).withValidation({
+          'foos': 'presence',
+          'foos.*.value': 'presence'
+        });
+
+        const setState = () => {
+          set('foos', [{}, {value: 1}])
+        };
+
+        return (
+          <div>
+            { get('foos').map((_item, i) => (
+                <Input key={ i } { ...$(`foos.${i}.value`) } wrapperClassName={ `foo-${i}` } />
+              ))
+            }
+            { getError('foos') &&
+              <div className="foos-error">{ getError('foos') }</div>
+            }
+            <button onClick={ setState } className="setState">Set State </button>
+            <button onClick={ validate } className="validate">Validate</button>
+          </div>
+        );
+      }
+
+      it('allows to use wildcard validation', () => {
+        const wrapper = mount(<Form />);
+        wrapper.find('.validate').simulate('click');
+        expect(wrapper.find('.foos-error')).to.have.lengthOf(1);
+        wrapper.find('.setState').simulate('click');
+        expect(wrapper.find('.foos-error')).to.have.lengthOf(0);
+        expect(wrapper.find('.foo-0 .error')).to.have.lengthOf(1);
+      });
+    });
+
+    describe('callbacks usage', () => {
+      function Form({ onValid, onError }) {
+        const { $, validate } = useForm({ foo: '' }).withValidation({
+          foo: 'presence'
+        });
+
+        const save = () => {
+          validate({ onValid, onError });
+        };
+
+        return (
+          <div>
+            <Input { ...$('foo') } className="foo" />
+            <button onClick={ save } className="save">Save</button>
+          </div>
+        );
+      }
+
+      it('allows to use callbacks', () => {
+        const validSpy = sinon.spy();
+        const errorSpy = sinon.spy();
+        const wrapper = mount(<Form onValid={ validSpy } onError={ errorSpy } />);
+        wrapper.find('.save').simulate('click');
+        expect(validSpy).to.have.property('callCount', 0);
+        expect(errorSpy).to.have.property('callCount', 1);
+        wrapper.find('input.foo').simulate('change', { target: { value: '1' } });
+        wrapper.find('.save').simulate('click');
+        expect(validSpy).to.have.property('callCount', 1);
+      });
+    });
+  });
+});
