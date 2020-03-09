5a23ee08266d29e6f8bf766eba88623489173a60
Artem Kuzko <a.kuzko@gmail.com>
2020-03-10 00:20:00 +0200

    Fix stand-alone input validation
    - When fetching input value during stand-alone input validation, use
    `get-lookup` helper to correctly access nested values.

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
 import { validateAttr, validateRule, wildcard } from './validations';
 import { resolveConfig, mergeConfigs } from './config';
 import update from 'update-js';
+import get from 'get-lookup';
 
 export default function reducer(state, action) {
   const { attrs, errors, validations, validationOptions, validationDeps, configs } = state;
@@ -137,7 +138,7 @@ export default function reducer(state, action) {
     }
     case 'validatePath': {
       const { path, resolve, reject } = action;
-      const value = attrs[path];
+      const value = get(attrs, path);
       const fullOpts = { ...validationOptions, attrs };
       const error = validateAttr(validations, fullOpts, path, value);
 
