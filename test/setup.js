4b9dafccf1c613fd172ec1281df689200b1e948e
Artem Kuzko <a.kuzko@gmail.com>
2022-04-09 15:23:30 +0300

    Update dependencies to resolve vulnerabilities
    Description:
    - Update dependencies to resolve vulnerabilities reported by npm
    - Also, add `asdf`'s `.tool-versions` to `.gitignore` (at the moment
    of writing using nodejs 14.19.1)

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 import jsdom from 'jsdom';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 const { JSDOM } = jsdom;
 
